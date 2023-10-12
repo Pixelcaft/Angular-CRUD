0d927a934ef17c3041cdb1210aaccbd47a9b8af6
Pixelcaft <ryandegraaf2002@gmail.com>
2023-10-12 09:58:20 +0200

    required gefix

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { LocalstorageService } from 'src/app/services/localstorage.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/classes/user.class';
-import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -13,25 +12,12 @@ export class FormComponent {
   public buttonText: string = 'Add User';
   private isUpdateMode: boolean = false;
   public user!: User;
-  public userForm: FormGroup;
 
   constructor(
     private localStorageService: LocalstorageService,
     private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private formBuilder: FormBuilder
+    private activatedRoute: ActivatedRoute
   ) {
-    this.userForm = this.formBuilder.group({
-      firstname: ['', Validators.required],
-      infix: [''],
-      lastname: ['', Validators.required],
-      streetname: ['', Validators.required],
-      housenumber: ['', Validators.required],
-      addition: [''],
-      residence: ['', Validators.required],
-      postalcode: ['', Validators.required],
-    });
-
     this.activatedRoute.url.subscribe(() => {
       const userId = this.activatedRoute.snapshot.params['id'];
 
@@ -67,8 +53,8 @@ export class FormComponent {
   }
 
   public submitForm() {
-    if (this.userForm.valid) {
-      const UserData = this.userForm.value;
+    if (this.inFormValid()) {
+      const UserData = this.user;
 
       if (this.isUpdateMode) {
         const userId = this.activatedRoute.snapshot.params['id'];
@@ -81,6 +67,17 @@ export class FormComponent {
     } else {
       alert('Fill in all required fields before submitting the form.');
     }
+  }
+
+  private inFormValid(): boolean {
+    return (
+      !!this.user.firstname &&
+      !!this.user.lastname &&
+      !!this.user.streetname &&
+      !!this.user.housenumber &&
+      !!this.user.residence &&
+      !!this.user.postalcode
+    );
   }
 
   private backIndexPagina() {
